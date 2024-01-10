c5678ba561011ff59d908b1c2feb6b9d5bbd5020
Manas-Nigam <manasnigam5432@gmail.com>
2024-01-10 14:40:53 +0530

    Minor fix in skills image

--- components/constants/index.js
+++ components/constants/index.js
@@ -211,7 +211,7 @@ export const Backend_skill = [
 export const Full_stack = [
   {
     skill_name: "React Native",
-    Image: "/assets/ReactNative .png",
+    Image: "/assets/ReactNative.png",
     width: 70,
     height: 70,
   },
